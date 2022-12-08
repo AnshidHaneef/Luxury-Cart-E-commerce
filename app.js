bd8e3f3a90fb5ab7d9851c6fdbbbd830dc306d44
anshid haneef <anshidhaneefa710@gmail.com>
2022-12-08 16:54:16 +0530

    error handled

--- app.js
+++ app.js
@@ -93,8 +93,9 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
+
   res.status(err.status || 500);     
-  res.render('error');
+  res.render('/error');
 });
 
 app.listen(7000,()=>{
